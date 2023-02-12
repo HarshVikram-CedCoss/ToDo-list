49177d52cc5da08b1f3936e514a1665a6fbf5cf2
HarshVikram-CedCoss <harshvikramsingh88@gmail.com>
2023-02-13 00:28:46 +0530

    setup the initial code

---
+++ src/components/TodoForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+const TodoForm = (props) => {
+  return (
+    <form className="todoForm" onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        value={props.todo}
+        onChange={(e) => props.setTodo(e.target.value)}
+      />
+      <button type="submit"> {props.editId ? "Edit" : "Go"}</button>
+    </form>
+  );
+};
+
+export default TodoForm;
